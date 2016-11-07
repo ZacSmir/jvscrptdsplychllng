4c3477ae53f26c565c1c8c6410beb04c0e631524
ZacSmir <Zach@smirin.us>
2016-11-07 08:46:57 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ function logArgs(args) {
   console.log(args)
 }
 
+console.log(logArgs.toString())
+
 var functionString = "how do I get the function as a string?"
 
 var consoleLogExample = $("how do I get the element where I want to put the example?")
 
 var formattedExample = $("<pre>")
+
+
 
 formattedExample.text(contents)
 
