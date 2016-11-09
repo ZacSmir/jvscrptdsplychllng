14868930d584f7eef51d451ac7636e85410ee645
ZacSmir <Zach@smirin.us>
2016-11-09 09:06:44 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-// demo function
+// CONSOLE.LOG
 function logArgs(args) {
   var a = 1
   console.log(args)
 }
 
+var usethis = logArgs.toString()
+
+var formattedExample = $("<pre>")
+
+formattedExample.text(usethis)
+
+$("#prething").append(formattedExample)
+//-------------------------
+//FUNCTIONS
 function math(number){
 return number + 1
 }
-
-var usethis = logArgs.toString()
 
 var functionsusethis = math.toString()
 
 var otherformattedExample = $("<pre>")
 
-var formattedExample = $("<pre>")
-
 otherformattedExample.text(functionsusethis)
-
-formattedExample.text(usethis)
 
 $("#functionthing").append(otherformattedExample)
 
-$("#prething").append(formattedExample)
